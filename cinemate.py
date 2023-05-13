4562245c2efa3aa376c018ff847f0f461370789d
CarlMurray <carl.j.murray@gmail.com>
2023-05-13 20:23:34 +0100

    refactor: implement Movie class

--- cinemate.py
+++ cinemate.py
@@ -3,30 +3,48 @@ import csv
 
 DATASET = "testsheet.tsv" # FILE PATH FOR IMDB DATASET
 fav_list = [] # LIST OF USER FAVOURITES
+list_headers = f'{"#":<5}{"Title":<50}{"Release":<10}{"Runtime":<10}{"Genre":<10}' # LIST HEADER FORMATTING
+
+# MOVIE CLASS
+class Movie:
+    def __init__(self, index, title, date, runtime, genres):
+        self.index = index          # INDEX TO SHOW IN PRINTED LIST
+        self.title = title          # MOVIE TITLE
+        self.date = date            # RELEASE DATE
+        self.runtime = runtime      # MOVIE RUNTIME
+        self.genres = genres        # GENRES
+        
+    def __str__(self):
+        return f'{self.index:<5}{self.title:<50}{self.date:<10}{self.runtime:<10}{self.genres}'
 
 # GETS DATA FROM IMDB DATASET AND STORES IN LIST
-def get_data():
+def get_movies():
     with open(DATASET, "r") as f:
         tsv_f = csv.DictReader(f, delimiter="\t")
-        data = []
+        movies = []
+        index = 0
         for row in tsv_f:
+            index += 1
             if row["genres"] == '\\N':
                 row["genres"] = '-' # OVERRIDE DEFAULT INVALID GENRE STRING
-            data.append(row)
-        return data
+            title = row["primaryTitle"]
+            date = row["startYear"]
+            runtime = row["runtimeMinutes"]
+            genres = row["genres"]
+            movie = Movie(index, title, date, runtime, genres)
+            movies.append(movie)
+    return movies
 
 # SHOWS MOVIE LIST
 def show_movies():
-    index = 0 # SETS INITIAL INDEX TO 0
-    data = get_data()
+    movies = get_movies()
 
-    # PRINT COLUMN HEADERS
-    print(f'{"#":<5}{"Title":<50}{"Release":<10}{"Runtime":<10}{"Genre":<10}')
+    # PRINT LIST HEADERS
+    print(list_headers)
 
     # PRINT EACH MOVIE
-    for movie in data:
-        index += 1
-        print(f'{index:<5}{movie["primaryTitle"]:<50}{movie["startYear"]:<10}{movie["runtimeMinutes"]:<10}{movie["genres"]}')
+    for movie in movies:
+        print(movie)
     select_user_action()
 
 # HOME MENU FOR USER SELECTION
@@ -49,18 +67,17 @@ def get_selection():
 # SHOW FAV LIST
 def show_favourites():
     
-    # PRINT ROW HEADERS
-    print(f'{"#":<5}{"Title":<50}{"Release":<10}{"Runtime":<10}{"Genre":<10}')
+    # PRINT LIST HEADERS
+    print(list_headers)
     
     index = 0 # SETS INITIAL INDEX TO 0
     
     # PRINT LIST OF FAVOURITES
-    for fav in fav_list:
+    for movie in fav_list:
         index += 1
-        if fav["genres"] == '\\N':
-            fav["genres"] = '-'
-        print(f'{index:<5}{fav["primaryTitle"]:<50}{fav["startYear"]:<10}{fav["runtimeMinutes"]:<10}{fav["genres"]}')
-    
+        movie.index = index
+        print(movie)
+        
     # SHOW OPTIONS MENU
     print('\nPlease select an option from the menu below:')
     print('1 - Remove favourite')
@@ -80,7 +97,7 @@ def remove_favourite():
     removed = fav_list.pop(selection)
     
     # SHOW FEEDBACK MSG
-    print(f'\n{removed["primaryTitle"]} removed from favourites\n')
+    print(f'\n{removed.title} removed from favourites\n')
     
     # SHOW UPDATED FAVOURITES LIST
     show_favourites()
@@ -103,13 +120,13 @@ def select_user_action():
         
 # FOR USER TO ADD A FAVOURITE TO LIST
 def add_favourite():
-    movies_list = get_data()
+    movies = get_movies()
     
     # ENTER # OF MOVIE
     favourite = int(input('\nType the ID of the movie and press enter to add favourite: ')) - 1
     
     # ADD CHOICE TO FAV LIST
-    fav_list.append(movies_list[favourite])
+    fav_list.append(movies[favourite])
     
     # PROMPT USER FOR Y/N TO CONTINUE
     user_continue = input('\nDo you want to add another favourite? ')
@@ -132,5 +149,5 @@ def add_favourite():
 
     return fav_list
 
-get_data()
+get_movies()
 home_menu()
