c1c8d0ffc4d4766640f3701a43832b6513695280
CarlMurray <carl.j.murray@gmail.com>
2023-05-14 20:11:12 +0100

    trim dataset - top 1000 movies

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # CHOOSE PATH FOR NEW TSV OUTPUT
-OUTPUT_PATH = '/Users/carl.murray/Documents/cinemate-pp3/movie_data.tsv'
+OUTPUT_PATH = 'ENTER A FILEPATH/movie_data.tsv' 
 
 # OPEN IMDB TSV FILES
 df_basics = pd.read_table('title.basics.tsv', low_memory=False)
@@ -24,8 +24,8 @@ df_basics['runtimeMinutes'] = df_basics['runtimeMinutes'].astype(int)
 
 # FILTER BY DATE AND RUNTIME
 df_basics = df_basics.loc[
-    (df_basics['runtimeMinutes'] > 100) &
-    (df_basics['startYear'] > 2015)
+    (df_basics['runtimeMinutes'] > 90) &
+    (df_basics['startYear'] > 2000)
 ]
 
 # MERGE TSVs - ONLY MOVIES WITH RATINGS INCLUDED
@@ -33,6 +33,18 @@ combined = pd.merge(df_basics, df_ratings, on='tconst')
 
 # DEFINE OUTPUT COLUMNS
 columns = ['primaryTitle', 'startYear', 'runtimeMinutes', 'genres', 'averageRating', 'numVotes']
+
+# SET RATING AND VOTES CRITERIA
+combined = combined.loc[
+    (combined['numVotes'] > 50000) & 
+    (combined['averageRating'] >= 7.0)
+]
+
+# SORT BY NUMVOTES
+combined = combined.sort_values(by=['numVotes'], ascending=False)
+
+# TRIM TO TOP 1000 MOVIES
+combined = combined.head(1000)
 
 # OUTPUT TO NEW TSV FILE
 combined.to_csv(OUTPUT_PATH , sep='\t', header=True, index=False, columns=columns)
